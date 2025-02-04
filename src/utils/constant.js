2eb26cfa215789f78874e19ddab2be542648d8ce
Jay Prakash <jayprakash1200@gmail.com>
2025-02-04 17:03:29 +0530

    regsiter for tmdb api and create a an application to get access token and get data from tmdb now playing movie

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,12 @@
-export const LOGO = "https://images.ctfassets.net/y2ske730sjqp/821Wg4N9hJD8vs5FBcCGg/9eaf66123397cc61be14e40174123c40/Vector__3_.svg?w=460"
-export const PHOTO_URL = "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg"
+export const LOGO =
+  "https://images.ctfassets.net/y2ske730sjqp/821Wg4N9hJD8vs5FBcCGg/9eaf66123397cc61be14e40174123c40/Vector__3_.svg?w=460";
+export const PHOTO_URL =
+  "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg";
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OGIzMmRlY2UzZDg0Y2IyNTNjOTc0Y2ExNmE0NDVmNiIsIm5iZiI6MTczODY2NjEzMC4wMzEwMDAxLCJzdWIiOiI2N2ExZjA5MmE0NTI4N2I3ZmRlMmQzOTUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.9-Hcz45ohQjwXM9-wfXxo9cAfUFxcLhN-2TKGf21x_A",
+  },
+};
